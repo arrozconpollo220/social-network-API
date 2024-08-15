c90ce0032ccc0f4601102775fd41a6572c9f8a0f
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-08-14 20:44:04 -0600

    created and updated user.js

---
+++ models/user.js
@@ -0,0 +1,48 @@
+// Initiate the user model
+const { Shcema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get total count of friends on retrieval
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', userSchema);
+
+// export the User model
+module.exports = User;
