39138efeb14a79e03085801fe3b209c85931919b
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-08-19 17:43:03 -0600

    updated thought controller.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,86 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // Get all thoughts
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Get a thought
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Create a thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Delete a thought
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+
+            await User.findOneAndUpdate(
+                { username: thought.username },
+                { $pull: { thoughts: thought._id } }
+            );
+
+            res.json({ message: 'Thought deleted!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Update a thought
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Add a reaction
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $push: { reactions: req.body } },
+                { runValidators: true, new: true }
+            );
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
