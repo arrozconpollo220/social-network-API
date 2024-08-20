8261bb23adced38955a8e5131d66178eaa46d390
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-08-20 13:43:25 -0600

    updated user controller

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Get a user
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+            if (!user) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Create a user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    // Delete a user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+            if (!user) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+            await Thought.deleteMany({ username: user.username });
+            res.json({ message: 'User deleted!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Update a user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'Cannot find ID' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
